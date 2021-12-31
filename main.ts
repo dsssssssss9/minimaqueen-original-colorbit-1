c3263ad66d187fed50d1a5a5c1fe52d169946354
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-12-31 00:20:57 +0000

    use latest extension - https://github.com/51bit/ColorBit

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 input.onButtonPressed(Button.A, function () {
+    colorbit_51bit.setMultiColor(false)
     colorbit_51bit.clear()
-    colorbit_51bit.showScrollStringColor("Test", colorbit.colors(BitColors.Red))
+    colorbit_51bit.showScrollStringColor("Hello There Frank - - - I am Mini:MaQueen", colorbit.colors(BitColors.Blue))
     colorbit_51bit.show()
 })
 let colorbit_51bit: colorbit.Strip = null
@@ -8,6 +9,3 @@ colorbit_51bit = colorbit.initColorBit(DigitalPin.P1, BitColorMode.RGB)
 colorbit_51bit.clear()
 colorbit_51bit.setBrightness(16)
 colorbit_51bit.showColorIcon(ColorIcon.ChristmasTree, colorbit.colors(BitColors.Red))
-basic.forever(function () {
-	
-})
